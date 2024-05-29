38d23c391bfacacaef1a667d70b388ae58ac6fd2
NoSeUsarJS <113266693+NoSeUsarJS@users.noreply.github.com>
2024-05-29 00:47:37 -0400

    Update embedding.py

--- backend/services/embedding.py
+++ backend/services/embedding.py
@@ -1,6 +1,12 @@
-from openai import OpenAI
-client = OpenAI()
+from sentence_transformers import SentenceTransformer
+from typing import List
 
-def get_embeddings(text: str, model="text-embedding-3-small"):
-    text = text.replace("\n", " ")
-    return client.embeddings.create(input=[text], model=model).data[0].embedding
+def get_embeddings(text: str, model_name="all-MiniLM-L6-v2") -> List[float]:
+    model = SentenceTransformer(model_name)
+    text_length = len(text)
+
+    maxsplit = int(text_length/200) if text_length > 200 else 1
+
+    texts = text.split(maxsplit=maxsplit)
+
+    return model.encode(texts).tolist()
