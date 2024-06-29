ae2a0087c5a40ba529a9605f66895e07c0add06f
Javier Molina <113266693+javierignma@users.noreply.github.com>
2024-06-29 04:05:33 -0400

    CORS fixed

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,21 @@ from typing import List
 from persistence.milvus_manager import MilvusManager
 from fastapi import FastAPI, File, UploadFile, Form
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3001"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
+)
 
 class Query(BaseModel):
     text: str
